dd91c8dad454c07079031d63e6df736a827eac96
BradyFoster <Bjfoster9860@gmail.com>
2023-06-16 11:08:14 -0700

    Complete palindrome assignment

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if (word === word.split('').reverse().join('')) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
+  function isPalindrome takes a string as an argument. If the string is a palindrome then return true. If the string is not a palindrome return false.
 */
 
 /*
-  Add written explanation of your solution here
+  The function takes the word and splits each letter in to an array, then the word is reversed and joined back into a word. If the word is the same backwards as it is forwards then it returns true. If not then it is returned false.
 */
 
 // You can run `node index.js` to view these console logs
